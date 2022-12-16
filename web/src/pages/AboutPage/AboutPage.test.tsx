0f6de3b86646c094c26b0a7ce28e8c7ab56c80fb
Eddie Gómez <41875751+iameddieyayaya@users.noreply.github.com>
2022-12-16 12:24:31 -0800

    add about/contact/waitinglist page stubs + footer routing (#8)

---
+++ web/src/pages/AboutPage/AboutPage.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@redwoodjs/testing/web'
+
+import AboutPage from './AboutPage'
+describe('AboutPage', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<AboutPage />)
+    }).not.toThrow()
+  })
+})
